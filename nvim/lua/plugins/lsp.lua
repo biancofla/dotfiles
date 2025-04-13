1afc78f4834e7d73786aecc043661fc2e853a74d
biancofla <flavio.biancospino+github@gmail.com>
2025-04-13 18:30:45 +0200

    lsp: add pyright support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,34 +1,36 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",       -- Lua
-	  "ruff",	  -- Python
-	  "eslint",	  -- TypeScript
-          "rust_analyzer" -- Rust
-        }
-      })
-    end
-  },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    "ruff",
+                    "eslint",
+                    "rust_analyzer"
+                }
+            })
+        end
+    },
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require('lspconfig')
 
-      lspconfig.lua_ls.setup ({})
-      lspconfig.ruff.setup({})
-      lspconfig.eslint.setup({})
-      lspconfig.rust_analyzer.setup({})
-    end
-  }
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.ruff.setup({})
+            lspconfig.eslint.setup({})
+            lspconfig.rust_analyzer.setup({})
+        end
+    }
 }
