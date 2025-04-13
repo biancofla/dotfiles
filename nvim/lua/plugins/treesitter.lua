c4d345c1c1b4e7e55b69669242707e9a781f9001
biancofla <flavio.biancospino+github@gmail.com>
2025-04-13 18:30:17 +0200

    plugin (treesitter): update conf

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         opts = {
             highlight = { enable = true },
             indent = { enable = true },
