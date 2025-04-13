eb25f383d31363e64a3d13472ff0090e6511a69a
biancofla <flavio.biancospino+github@gmail.com>
2025-04-13 21:48:41 +0200

    plugin (tiny-inline-diagnostic): update conf

--- nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -4,8 +4,7 @@ return {
         event = "VeryLazy",
         priority = 1000,
         config = function()
-            require('tiny-inline-diagnostic').setup()
-            vim.diagnostic.config({ virtual_text = false })
+            require('tiny-inline-diagnostic').setup({})
         end,
     }
 }
